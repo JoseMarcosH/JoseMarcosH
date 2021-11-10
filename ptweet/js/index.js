e39f8cdc51f0b1038fe40f64cc232e2eccfb22d5
JoseMarcosH <74742536+JoseMarcosH@users.noreply.github.com>
2021-11-09 22:06:45 -0700

    Elementos Dinamicos

--- ptweet/js/index.js
+++ ptweet/js/index.js
@@ -1,5 +1,44 @@
 window.onunload=()=>{
     let usuario= JSON.parse(localStorage.getItem("usuario"));
-    console.log("usuario");
-    document,getElementById("usuario").innerHTML="hola"+usuario.nombre
+    document.getElementById("imgUser").src="./img/"+usuario.imagen;
+    let arrayTw=[];
+    //console.log("usuario");
+    ///document,getElementById("usuario").innerHTML="hola"+usuario.nombre
+    let txtTweet=document.getElementById("txtTweet");
+    let btnTweet = document.getElementById("btnTweet");
+    btnTweet.addEventListener("click",()=>{
+        if(txtTweet.ariaValueMax.trim() != ""){
+            let obj = {
+                img: usuario.imagen,
+                nombre:usuario.nombre,
+                mensaje:txtTweet.value,
+                username:usuario.username,
+            }
+            arrayTw.push(obj);
+            crearPost(obj);
+            txtTweet.value="";
+        }
+    });
+    txtTweet.addEventListener("keyup",(evt)=>{
+
+    });
+    function crearPost(){
+        arrayTw.forEach(el=>{
+            todo+=`<div class="post">
+            <div>
+                <img src="img/${el.img}" alt="" class="imgUser">
+            </div>
+            <div>
+                <h2> 
+                    <span>${el.nombre}</span>
+                    <span>@${el.username}</span>
+                </h2>
+                <div class="textTweet">
+                    ${el.mensaje}
+                </div>
+            </div>
+        </div>`;
+        });
+        document.getElementById("posts").innerHTML=todo;
+    }
 }
