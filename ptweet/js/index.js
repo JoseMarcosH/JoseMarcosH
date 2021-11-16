466138a658ece10c2584b990cb32e4337f3579bc
JoseMarcosH <74742536+JoseMarcosH@users.noreply.github.com>
2021-11-16 10:49:32 -0700

    practica web

--- ptweet/js/index.js
+++ ptweet/js/index.js
@@ -2,6 +2,18 @@ window.onunload=()=>{
     let usuario= JSON.parse(localStorage.getItem("usuario"));
     document.getElementById("imgUser").src="./img/"+usuario.imagen;
     let arrayTw=[];
+    var xobj= new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET','./js/tweets.json',true);
+    xobj,onreadystatechange=function(){
+        if(xobj.readyState == 4 && xobj.status =='200'){
+            arrayTw=JSON.parse(xobj.responseText); 
+            console.log(arrayTw);
+            crearPost();
+        }
+    }
+    xobj.send(null);
+    
     //console.log("usuario");
     ///document,getElementById("usuario").innerHTML="hola"+usuario.nombre
     let txtTweet=document.getElementById("txtTweet");
@@ -13,6 +25,8 @@ window.onunload=()=>{
                 nombre:usuario.nombre,
                 mensaje:txtTweet.value,
                 username:usuario.username,
+                likes:0,
+                retweets:0
             }
             arrayTw.push(obj);
             crearPost(obj);
@@ -36,9 +50,38 @@ window.onunload=()=>{
                 <div class="textTweet">
                     ${el.mensaje}
                 </div>
+                <div id="icons">
+                    <div>
+                        <i class="fa fa-heart"></i> ${el.likes}
+                    </div>
+                    <div>
+                        <i class="fa fa-share"></i> ${el.retweets}
+                    </div>
+                </div>
+            </div>
+        </div>`;
+        el.comentarios.forEach(comentario=>{
+            todo+=`<div class="comentarios">
+            <div>
+                <img src="./img/${comentario.img}" alt="" class="imgUser">
+            </div>
+            <div>
+                <h2>
+                    <span>${comentario.nombre}</span>
+                    <span>${comentario.username}</span>
+                </h2>
+                <div class="textTweet">
+                    ${comentario.comentario}
+                </div>
             </div>
         </div>`;
         });
+        todo+=`
+            </div>
+        </div>
+        `
+        });
         document.getElementById("posts").innerHTML=todo;
+
     }
 }
