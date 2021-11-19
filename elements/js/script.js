5b1acdde593962f852b049ecd381049be22c7301
JoseMarcosH <74742536+JoseMarcosH@users.noreply.github.com>
2021-11-19 05:20:36 -0700

    Atributos Dinámicos y Evaluación Unidad 3

---
+++ elements/js/script.js
@@ -0,0 +1,38 @@
+window.onload = ()=>{
+    //CARGAR JSON
+    let arrayElem=[];
+
+    var xobj= new XMLHttpRequest();
+    xobj.overrideMimeType("aplication/json");
+    xobj.open('GET','./js/PeriodicTableJSON.json',true);
+    xobj.onreadystatechange=function(){
+        if(xobj.readyState == 4 && xobj.status=='200'){
+            let json=JSON.parse (xobj.responseText);
+            arrayElem=json.elements;
+            let todo="";
+            arrayElem.forEach(element => {
+                todo+=`<li class="hydrogen" data-="1" data-sim="H" 
+                data-name="Hidrogeno"
+                data-descr="el elemento escencial">
+                <abbr title="Hydrogen">${element.symbol}</abbr>
+            </li>`
+            });
+            document.getElementsByClassName('periodic-table')[0].innerHTML=todo;
+        }
+    }
+    xobj.send(null);
+
+
+    //CREAR EVENTOS
+    let elem= document.getElementsByTagName('li');
+    for(let x=0;x<elem.length;x++){
+        elem[x].addEventListener('click',(e)=>{
+            let nom=elem[x].getAttribute("data-name")
+            let des=elem[x].getAttribute("data-descr")
+            let sim=elem[x].getAttribute("data-sim")
+            document.getElementById("txtElemento").innerText=nom
+            document.getElementById("txtDescrip").innerText=des
+            document.getElementById("txtSimbolo").innerText=sim
+        });
+    };
+}
